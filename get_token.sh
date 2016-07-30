3e6d7a1c67cc1112dd5e0c3bccc384535a272a0a
danznz <danz@danz-pro.local>
2016-07-30 19:28:51 +1200

    Updated dirname logic

--- get_token.sh
+++ get_token.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd "$(dirname "$(readlink  "$(command -v "$0")")")"
+cd "$(dirname "$0")"
 curl 'http://solarview.niwa.co.nz/dosignin' -s -D - \
 --data 'username=Omnidatum&password=Delete123&domain=public' \
 | grep Set-Cookie|egrep -o 'symfony=(\w*)'|tail -1 > auth_token
