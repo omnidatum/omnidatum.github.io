586e264586ebdb075db8d52b0656f7d0f3c165a6
Murphy Mulholland <Murphytekau@gmail.com>
2016-07-30 17:39:44 +1200

    php script

---
+++ get_energy.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$period = $_REQUEST['period'];
+$address = $_REQUEST['address'];
+$PWD = __DIR__;
+
+$address = escapeshellcmd($address);
+
+if ($period == 'month') {
+	echo shell_exec("python $PWD/get_csv.py '" .$address. "'| xargs curl -s | python $PWD/data_reduce_month.py")
+} else if ($period == 'year') {
+	echo shell_exec("python $PWD/get_csv.py '" .$address. "'| xargs curl -s | python $PWD/data_reduce.py")
+} else {
+	echo json_encode(['error' => 'wrong period']);
+}
