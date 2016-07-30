aebc221346b79be6db37b38dd4ca0ce8a9ba0b33
Murphy Mulholland <Murphytekau@gmail.com>
2016-07-30 17:42:57 +1200

    syntax

--- get_energy.php
+++ get_energy.php
@@ -8,9 +8,9 @@ $PWD = __DIR__;
 $address = escapeshellcmd($address);
 
 if ($period == 'month') {
-	echo shell_exec("python $PWD/get_csv.py '" .$address. "'| xargs curl -s | python $PWD/data_reduce_month.py")
+	echo shell_exec("python $PWD/get_csv.py '" .$address. "'| xargs curl -s | python $PWD/data_reduce_month.py");
 } else if ($period == 'year') {
-	echo shell_exec("python $PWD/get_csv.py '" .$address. "'| xargs curl -s | python $PWD/data_reduce.py")
+	echo shell_exec("python $PWD/get_csv.py '" .$address. "'| xargs curl -s | python $PWD/data_reduce.py");
 } else {
 	echo json_encode(['error' => 'wrong period']);
 }
